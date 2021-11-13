bd8bd5ef8220c9a699eb3102e8fc287f8330ac8d
Artyom.Cherkasov <Artyom.Cherkasov@waveaccess.ru>
2021-11-13 22:07:18 +0400

    add journal

--- training-system/src/app/components/journal/journal.component.ts
+++ training-system/src/app/components/journal/journal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserDto} from "../../models/userDto";
+import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-journal',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./journal.component.less']
 })
 export class JournalComponent implements OnInit {
+  public show = true;
+  active = 1;
+  studentsInGroup: UserDto[] = [];
+  allStudents: UserDto[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private router: Router) {}
 
   ngOnInit(): void {
+    this.getUserInGroup();
+    this.getAllStudents();
+  }
+
+  public deleteUser(username: string): any{
+    this.userService.deleteStudentWithoutGroup(username)
+      .subscribe(() => {
+        this.getUserInGroup();
+        this.getAllStudents();
+      });
+  }
+
+  public getUserInGroup(): any{
+    this.userService.getStudentsInGroup()
+      .subscribe((data) => this.studentsInGroup = data);
+  }
+  public getAllStudents(): any{
+    this.userService.getAllStudents()
+      .subscribe((data) => this.allStudents = data);
+  }
+  public addStudentToGroup(username: string): any {
+    this.userService.addUserToGroup(username)
+      .subscribe(() => {
+        this.getUserInGroup();
+        this.getAllStudents();
+      });
   }
 
 }
