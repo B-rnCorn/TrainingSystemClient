f15e3d31c82a133b69b48b52fade55f2a7889d8f
SeregeySulimenko <sergey-sulimenko@mail.ru>
2021-12-26 23:30:29 +0400

    inited task solution

---
+++ training-system/src/app/components/tasks/commands/command/command.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-command',
+  templateUrl: './command.component.html',
+  styleUrls: ['./command.component.less']
+})
+export class CommandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
