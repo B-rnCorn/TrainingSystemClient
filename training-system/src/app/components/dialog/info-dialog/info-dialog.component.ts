5a2426b9cdbfd008cb829dc25e434ee9f7cc6973
4abrec <63412490+4abrec@users.noreply.github.com>
2021-12-28 02:00:11 +0400

    added play monkey

---
+++ training-system/src/app/components/dialog/info-dialog/info-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-dialog',
+  templateUrl: './info-dialog.component.html',
+  styleUrls: ['./info-dialog.component.less']
+})
+export class InfoDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
